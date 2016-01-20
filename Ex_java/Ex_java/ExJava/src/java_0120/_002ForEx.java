46a5a67d31e1aaf174c33e82f6ba3a54ab4d0b82
kh <jjanggu1990@gmail.com>
2016-01-20 20:02:43 +0900

    배열 에제 _003ArrayEx.java 추가함

--- Ex_java/Ex_java/ExJava/src/java_0120/_002ForEx.java
+++ Ex_java/Ex_java/ExJava/src/java_0120/_002ForEx.java
@@ -1,5 +1,7 @@
 package java_0120;
 
+import java.util.concurrent.SynchronousQueue;
+
 class _002ForEx {
 
 	public static void main(String[] args) {
@@ -95,5 +97,74 @@ class _002ForEx {
 		for(i=0; i<=(n/2);i++){
 			System.out.print(i*2+" ");
 		}
+		
+		/*
+		 * while 문 예제
+		 * args[3] : 
+		 */
+		
+		System.out.println("\n\nwhile 문 예제");
+		sum = i = 0;
+		int j = Integer.parseInt(args[3]);
+		while(i<=j){
+			sum += i;
+			i++;
+		}
+		System.out.println("1~"+ j + "까지의 합 : " + sum);
+		
+		
+		/*
+		 * do while 문 예제
+		 * while 문 후미에 ; 잊지 않고 찍기
+		 * 
+		 */
+		System.out.println("\n\ndo while 문 예제");
+		i = 5;
+		String str = "Java DoublePlus";
+		do{
+			System.out.println(str);
+		}while(i-- > 10);
+		
+		
+		/*
+		 * 
+		 * break 예제
+		 * label : 반복문에 이름을 붙이는 것
+		 * 
+		 */
+		System.out.println("\n\nbreak 예제");
+		label: 
+			for(i = 0;i<3;i++){
+				for(j = 0; j<5;j++){
+					if (j==3)
+						break label;
+					System.out.println("i값 : " + i + "  j값 : "+j);
+				}
+			}
+		
+		
+		/*
+		 * continue 예제
+		 * continue 와 break 문 이하의 문장은 처리 안함
+		 */
+		System.out.println("\n\ncontinue 문 예제");
+		for(i = 0;i<10;i++){
+			if (i%4 != 0)
+				continue;
+			System.out.println("i값 : " + i);
+		}
+		System.out.println("\n\nlebel이 붙은 continue");
+		label: 
+			for(i = 0;i<5;i++){
+				for(j = 0; j<5;j++){
+					if (j==3)
+						continue label;
+					System.out.println("i값 : " + i + "  j값 : "+j);
+				}
+			}
+		
+		
+		
+		
 	}
 }
---
+++ Ex_java/Ex_java/ExJava/src/java_0120/_003ArrayEx.java
@@ -0,0 +1,14 @@
+package java_0120;
+
+class _003ArrayEx {
+
+	public static void main(String[] args) {
+		/*
+		 * 
+		 * 변수 : 하나의 값을 저장 하는 공간
+		 * 배열 : 같은 타입의 여러개의 값을 모아서 저장하는 공간
+		 * 
+		 */
+	}
+
+}
